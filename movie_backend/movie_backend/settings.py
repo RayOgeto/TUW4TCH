07c1b199240f9cd82f7c0492e84d95d096cc44ff
Ray Ogeto <rayogeto40@gmail.com>
2024-07-27 22:51:13 +0300

    Update settings.py

--- movie_backend/movie_backend/settings.py
+++ movie_backend/movie_backend/settings.py
@@ -31,12 +31,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tz^+m@jkte077#+xhv@y)m)@sb3heaya#cf^jj0+*ffndr=qdb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#'django-insecure-tz^+m@jkte077#+xhv@y)m)@sb3heaya#cf^jj0+*ffndr=qdb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
