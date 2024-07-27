68cb89d0862343182b2a9942acdfca24f5a2014c
Ray Ogeto <rayogeto40@gmail.com>
2024-07-27 22:19:31 +0300

    trayol 3

--- movie_backend/movie_backend/settings.py
+++ movie_backend/movie_backend/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-tz^+m@jkte077#+xhv@y)m)@sb3heaya#cf^jj0+*ffndr=qdb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tuw4tch.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -97,13 +97,7 @@ WSGI_APPLICATION = 'movie_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    
-}
+
 
 
 # Password validation
@@ -147,3 +141,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
